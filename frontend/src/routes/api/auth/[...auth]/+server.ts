7eb4873a2fc332619794629fa2512ac04c1cca2d
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-05-24 16:07:20 +0800

    fix: auth server proxy

--- frontend/src/routes/api/auth/[...auth]/+server.ts
+++ frontend/src/routes/api/auth/[...auth]/+server.ts
@@ -1,4 +1,10 @@
-import { auth } from "$lib/server/auth";
+// import { auth } from '$lib/server/auth';
 
-export const GET = ({ request }) => auth.handler(request);
-export const POST = ({ request }) => auth.handler(request);
+// export const GET = ({ request }) => auth.handler(request);
+// export const POST = ({ request }) => auth.handler(request);
+
+import { auth } from '$lib/server/auth';
+import { toSvelteKitHandler } from 'better-auth/svelte-kit';
+
+export const GET = toSvelteKitHandler(auth);
+export const POST = toSvelteKitHandler(auth);
